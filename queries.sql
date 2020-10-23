a171c149810cf841a03436ac3da19a7325100d5b
Stephen Goodrick <sgoodrickjr@gmail.com>
2020-10-22 21:36:06 -0600

    List first 10 orders placed, sorted descending by the order date

--- queries.sql
+++ queries.sql
@@ -12,8 +12,9 @@ WHERE SupplierID = 11;
 
 -- List first 10 orders placed, sorted descending by the order date
 
-SELECT OrderID, OrderDate FROM [Orders]
-WHERE OrderID < 10258;
+SELECT * FROM [Orders]
+ORDER BY OrderDate DESC
+LIMIT 10;
 
 -- Find all customers that live in London, Madrid, or Brazil
 

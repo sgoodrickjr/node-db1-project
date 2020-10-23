780aa85c89fc50fd4acbcee68b4827e91cc2c3fe
Stephen Goodrick <sgoodrickjr@gmail.com>
2020-10-22 22:11:54 -0600

    (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted

--- queries.sql
+++ queries.sql
@@ -36,4 +36,7 @@ WHERE ContactName = 'Bilbo Baggins';
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
+SELECT COUNT ( DISTINCT City ) AS "Number of Unique Cities"
+FROM Customers;
+
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
